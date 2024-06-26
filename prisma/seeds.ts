f183f885bb1232a563d5a20402824ec068f49e84
Aaron Hales <a02353892@usu.edu>
2024-06-26 15:16:03 -0600

    changed department 1 to IT

--- prisma/seeds.ts
+++ prisma/seeds.ts
@@ -22,8 +22,51 @@ async function main() {
     }
   })
 
-  await prisma.department.create({
-    data: {
+  await prisma.department.upsert({
+    where: {
+      id: 1,
+    },
+    create: {
+      name: "Information Technology",
+      abbreviation: "IT",
+      departmentCode: "DPINFT",
+      phoneNumbers: {
+        connectOrCreate: {
+          where: {
+            id: 1,
+            phoneNumber: "4357974357"
+          },
+          create: {
+            phoneNumber: "4357974357",
+          }
+        }
+      },
+      email: "servicedesk@usu.edu",
+      subDepartments: {
+        connectOrCreate: {
+          where: {
+            id: 2,
+            name: "Service Desk"
+          },
+          create: {
+            name: "IT Service Desk",
+            email: "servicedesk@usu.edu",
+            phoneNumbers: {
+              connectOrCreate: {
+                where: {
+                  id: 1,
+                  phoneNumber: "4357974357",
+                },
+                create: {
+                  phoneNumber: "4357974357",
+                }
+              }
+            },
+          }
+        }
+      }
+    },
+    update: {
       name: "Information Technology",
       abbreviation: "IT",
       departmentCode: "DPINFT",
